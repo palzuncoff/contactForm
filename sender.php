0bde80e66131594e29a48952455176a20d1e0e5d
palzuncoff <palzuncoff@yahoo.com>
2017-05-24 15:01:26 +0300

    fancy mail

--- sender.php
+++ sender.php
@@ -1,17 +1,21 @@
 <?php
 
 require 'PHPMailer/PHPMailerAutoload.php';
+require_once 'php.php';
+require_once 'html.php';
+require_once 'javascript.php';
+require_once 'introduction.php';
 
 $mail = new PHPMailer;
 
 $mail->isSMTP();
-$mail->Host = "smtp.host.com";
+$mail->Host = "mail.host.com";
 $mail->SMTPAuth = true;
 $mail->Username = "office@host.com";
 $mail->Password = 'pass';
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
-$mail->setFrom("office@host.com", 'PBBot');
+$mail->setFrom("office@host.com", 'Positron Bohemia');
 $mail->isHTML(true);
 $mail->addReplyTo("office@host.com", 'Information');
 
@@ -32,30 +36,71 @@ if ($_SERVER["CONTENT_TYPE"] ==  'application/json') {
 
 
   if(empty($errors)){
-    $message  = "Имя пользователя: " . sanitizeString($data['name']) . "<br/>";
-    $message .= "E-mail пользователя: " . sanitizeString($data['email']) . "<br/>";
-    $message .= "Текст письма: " . sanitizeString($data['message']). "<br/>";
+    $senderName = sanitizeString($data['name']);
+    $message  = "<body style='padding: 3em; margen: 3em; background: #4285f4;'><div style='padding: 3em; margen: 3em; background: #fff; box-shadow: 0 5px 10px rgba(0,0,0,.15);'><h1 style='font-size: 1.5em; display: inline-block;'>Имя:</h1> " . $senderName . "<br/>";
+    $message .= "<h1 style='font-size: 1.5em; display: inline-block;'>E-mail:</h1> " . sanitizeString($data['email']) . "<br/>";
+    $message .= "<h1 style='font-size: 1.5em; display: inline-block;'>Текст:</h1> " . sanitizeString($data['message']). "<br/>";
     if ($data['JavaScript'] || $data['PHP'] || $data['HTMLCSS']) {
       $message .= "JavaScript: " . $data['JavaScript']. "<br/>";
       $message .= "PHP: " . $data['PHP']. "<br/>";
       $message .= "HTML CSS: " . $data['HTMLCSS'];
     }
+    $message .= "</div></body>";
     if (send_mail($message)) {
       $msg_box = "<span style='color: green;'>Message sent successfully!</span>";
 
 //Resive Message
-      $mail->addAddress(sanitizeString($data['email']), sanitizeString($data['name']));
+      $mail->addAddress(sanitizeString($data['email']), $senderName);
 
       if ($data['JavaScript'] || $data['PHP'] || $data['HTMLCSS']) {
-        $mail->Subject = '<meta charset="UTF-8"> <h1>Вы записались на курсы от Positron Bohemia</h1>';
-        $mail->Body    = 'Здравствуйте '. sanitizeString($data['name']).
-        ' Вы записались на следующие курся от Positron Bohemia'. "<br/>"
-        . "JavaScript: " . $data['JavaScript']. "<br/>".
-        "PHP: " . $data['PHP']. "<br/>".
-        "HTML/CSS: " . $data['HTMLCSS'];
+        $mail->Subject = 'Positron Bohemia Courses';
+        $course = "";
+        if ($data['JavaScript']) $course .= $js;
+        if ($data['PHP']) $course .= $php;
+        if ($data['HTMLCSS']) $course .= $html;
+// subscribe curses
+        $mail->Body = $intro.
+        "<body leftmargin='0' marginwidth='0' topmargin='0' marginheight='0' offset='0' bgcolor='' class='background'>
+        <table align='center' border='0' cellpadding='0' cellspacing='0' height='100%' width='100%' class='background'>
+          <tr>
+            <td align='center' valign='top' width='100%' class='background'>
+              <center>
+                <table cellpadding='0' cellspacing='0' width='600' class='wrap'>
+                  <tr>
+                    <td valign='top' class='wrap-cell' style='padding-top:30px; padding-bottom:30px;'>
+                      <table cellpadding='0' cellspacing='0' class='force-full-width'>
+                        <tr>
+                         <td height='60' valign='top' class='header-cell'>
+                            <a class='logo' href='http://host.com/' target='_blank'>Positron Bohemia</a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td valign='top' class='body-cell'>
+                          <h1 style='color:#fff'>Привет ".$senderName."!</h1>".$course."
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+                <tr>
+                  <td valign='top' class='footer-cell'>
+                    Positron Bohemia<br>
+                    Hope you are having a great day so far!
+                  </td>
+                </tr>
+              </tr>
+            </table>
+            </center>
+            </td>
+            </tr>
+            </table>
+          </body>
+        </html>";
+// end suscribe
       } else {
         $mail->Subject = 'Thank you for contacting us.';
-        $mail->Body    = 'Hello '. sanitizeString($data['name']). 'Thank you!';
+// contact us
+        $mail->Body = "<body style='padding: 3em; margen: 3em; background: #4285f4;'><div style='padding: 3em; margen: 3em; background: #fff; box-shadow: 0 5px 10px rgba(0,0,0,.15);'><h1 style='margin: 0; font-size: 1.3em; color: #444; line-height: 2;'> Hello ". $senderName. "!<br> Thank you for your message on <a href='http://host.com/' target='_blank' style='color: #4285f4; text-decoration: none;'>Positron Bohemia</a><br> Hope you are having a great day so far!</h1><div></body>";
+// end contact us
       }
 
       if(!$mail->send()) {
@@ -82,11 +127,11 @@ if ($_SERVER["CONTENT_TYPE"] ==  'application/json') {
 
 
 function send_mail($message){
-  $mail_to = "one.of.as@gmail.com";
+  $mail_to = "exemple@gmail.com";
   $subject = "Письмо с обратной связи";
   $headers = "MIME-Version: 1.0\r\n";
   $headers .= "Content-type: text/html; charset=utf-8\r\n";
-  $headers .= "From: Письмо с positronbohemia.com <no-reply@test.com>\r\n";
+  $headers .= "From: Письмо с host.com <no-reply@test.com>\r\n";
   return mail($mail_to, $subject, $message, $headers);
 };
 
